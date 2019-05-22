03506c42029f8c85a7f8c5619ea9bb3f09a459f1
mafiqqq <afiqqqx1997@gmail.com>
2019-05-23 05:44:52 +0800

    Edit Profile

--- src/app/edit-profile/edit-profile.page.ts
+++ src/app/edit-profile/edit-profile.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { AuthenticateService } from '../services/authentication.service'; 
+import { ProfileService } from '../services/user/profile.service';
+
 
 @Component({
   selector: 'app-edit-profile',
@@ -8,13 +12,135 @@ import { Router } from '@angular/router';
 })
 export class EditProfilePage implements OnInit {
 
-  constructor(private router: Router) { }
+  public userProfile: any;
+  public birthDate: Date;
+  public contact: string;
+
+  constructor(
+    private alertCtrl: AlertController,
+    private authService: AuthenticateService,
+    private profileService: ProfileService,
+    private router: Router
+  ) { }
 
   profile() {
     this.router.navigateByUrl('/tabs/tab5');
   }
 
   ngOnInit() {
+    this.profileService
+    .getUserProfile()
+    .get()
+    .then( userProfileSnapshot => {
+      this.userProfile = userProfileSnapshot.data();
+      this.birthDate = userProfileSnapshot.data().birthDate;
+      this.contact = userProfileSnapshot.data().contact;
+    });
   }
+
+  async updateName(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      subHeader: 'Your full name',
+      inputs: [
+        {
+          type: 'text',
+          name: 'fullName',
+          placeholder: 'Your full Name',
+          value: this.userProfile.fullName,
+        }
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService.updateName(data.fullName);
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+
+   updateDOB(birthDate: string): void {
+    if (birthDate === undefined) {
+      return;
+    }
+    this.profileService.updateDOB(birthDate);
+  }
+
+  async updateContact(contact: string): Promise<any>{
+    const alert = await this.alertCtrl.create({
+      subHeader: 'Your Contact Number',
+      inputs: [
+        {
+          type: 'text',
+          name: 'contact',
+          placeholder: 'Your Contact Number',
+          value: this.userProfile.contact,
+        }
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService.updateContact(data.contact);
+          },
+        },
+      ],
+    });
+    await alert.present();
+
+  }
+
+  async updateEmail(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      inputs: [
+        { type: 'text', name: 'newEmail', placeholder: 'Your new email' },
+        { name: 'password', placeholder: 'Your password', type: 'password' },
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService
+              .updateEmail(data.newEmail, data.password)
+              .then(() => {
+                console.log('Email Changed Successfully');
+              })
+              .catch(error => {
+                console.log('ERROR: ' + error.message);
+              });
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+  
+  async updatePassword(): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      inputs: [
+        { name: 'newPassword', placeholder: 'New password', type: 'password' },
+        { name: 'oldPassword', placeholder: 'Old password', type: 'password' },
+      ],
+      buttons: [
+        { text: 'Cancel' },
+        {
+          text: 'Save',
+          handler: data => {
+            this.profileService.updatePassword(
+              data.newPassword,
+              data.oldPassword
+            );
+          },
+        },
+      ],
+    });
+    await alert.present();
+  }
+
 
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -51,6 +51,7 @@ export class RegisterPage implements OnInit {
        console.log(res);
        this.errorMessage = "";
        this.successMessage = "Your account has been created. Please log in.";
+       this.navCtrl.navigateBack('');
      }, err => {
        console.log(err);
        this.errorMessage = err.message;
